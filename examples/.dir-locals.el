14ae87f172d28703ae7076abdaf6d51d4b5ff71c
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-04-23 10:33:27 +1000

    Add .dir-locals to examples directory to get dante working.

---
+++ examples/.dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((haskell-mode
+  (dante-methods . (bare-ghci))
+  (dante-project-root . "examples")))
