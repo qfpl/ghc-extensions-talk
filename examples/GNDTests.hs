da901ff8cfaaac2eecc70aead9fbaec98b49171b
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-05-02 23:12:50 +1000

    Clean up GNDTests.

--- examples/GNDTests.hs
+++ examples/GNDTests.hs
@@ -2,14 +2,14 @@ module GNDTests where
 
 import Test.QuickCheck (Arbitrary (arbitrary), property, oneof)
 import Test.QuickCheck.Checkers (quickBatch, EqProp (..))
-import Test.QuickCheck.Classes (applicative)
+import Test.QuickCheck.Classes (applicative, monad)
 
 import GND
 
 instance Arbitrary a => Arbitrary (Maybe' a) where
   arbitrary = oneof [
       Just' <$> arbitrary
-    , (Just'' . IdentityT . Just) <$> arbitrary <*> arbitrary
+    , Just'' . IdentityT . Just <$> arbitrary <*> arbitrary
     , pure Nothing'
     ]
 
@@ -17,7 +17,9 @@ instance Eq a => EqProp (Maybe' a) where
   Nothing' =-= Nothing' = property True
   Just' x =-= Just' y = property $ x == y
   Just'' ia1 n1 =-= Just'' ia2 n2 = property (ia1 == ia2 && n1 == n2)
+  _ =-= _ = property False
 
 
-main =
-  quickBatch $ applicative (Just' ((), (42 :: Int), "hi"))
+main = do
+  quickBatch $ applicative (Just' ((), 42 :: Int, "hi"))
+  quickBatch $ monad (Just' ((), 42 :: Int, "hi"))
