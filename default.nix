5bddcc5c2d77c72d248f1b8f76f2e10bf9fcb9f0
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-05-14 15:31:37 +1000

    Add a conclusion.

--- default.nix
+++ default.nix
@@ -35,6 +35,7 @@ in
           $src/slides/records.md \
           $src/slides/heavier.md \
           $src/slides/type-classes.md \
+          $src/slides/conclusion.md \
           $src/slides/references.md \
           > slides.md
       pandoc -i -t revealjs --slide-level=2 --template=$src/template.revealjs --variable=codedir:$out --variable=transition:none --no-highlight -s slides.md -o index.html
---
+++ slides/conclusion.md
@@ -0,0 +1,8 @@
+# Conclusion
+
+##
+
+- Haskell 2010 is smaller than you think.
+- GHC defines many extensions to the language.
+- Language extensions come with tradeoffs and consequences.
+
